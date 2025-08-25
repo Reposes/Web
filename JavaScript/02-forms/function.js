ec04d36952dffab0f880c46bac6d68f2b84ae534
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2025-08-26 06:00:01 +1000

    Image Change Done

---
+++ JavaScript/02-forms/function.js
@@ -0,0 +1,23 @@
+// JavaScript source code
+function Power() {
+	let base = document.getElementById("base").value;
+	let exponent = document.getElementById("exponent").value;
+	document.getElementById("power").innerHTML = base ** exponent;
+}
+
+function GetMouseCoords(event) {
+	let x = event.clientX;
+	let y = event.clientY;
+	document.getElementById("mouse").innerHTML = `X = ${x}, Y = ${y}`;
+}
+document.addEventListener("mousemove", GetMouseCoords);
+
+function SwitchBackground() {
+	let switchBackground = document.getElementById('switch-background');
+	if (switchBackground.style.backgroundImage === 'url("img/moon.png")') {
+		switchBackground.style.backgroundImage = 'url("img/sun.png")';
+	}
+	else {
+		switchBackground.style.backgroundImage = 'url("img/moon.png")';
+	}
+}
